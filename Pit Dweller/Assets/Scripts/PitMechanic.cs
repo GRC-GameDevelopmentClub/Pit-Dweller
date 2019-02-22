ed9285e58d5991e822981c8cbb73ae3a1ebbcdc2
Howarddful123 <Howarddful123@gmail.com>
2019-02-22 12:43:34 -0800

    -Added a temp sprite for water. -Water can now move up and down.

--- Pit Dweller/Assets/Scripts/PitMechanic.cs
+++ Pit Dweller/Assets/Scripts/PitMechanic.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PitMechanic : MonoBehaviour {
+    [SerializeField]
+    private Transform pitWater;
+    [SerializeField]
+    private Vector3 posA;
+    [SerializeField]
+    private Vector3 posB;
+    [SerializeField]
+    private Vector3 nextPos;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Transform transformB;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        posA = pitWater.localPosition;
+        posB = transformB.localPosition;
+        nextPos = posB;    
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        pitWater.localPosition = Vector3.MoveTowards(pitWater.localPosition,nextPos,speed*Time.deltaTime);
+    }
 }
