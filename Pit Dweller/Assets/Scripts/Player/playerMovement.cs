9bfc5705d4f9bc17b430bec026d8d720a875bdc5
Howarddful123 <Howarddful123@gmail.com>
2019-03-01 15:08:51 -0800

    -Added dashing for spacebar.

--- Pit Dweller/Assets/Scripts/Player/playerMovement.cs
+++ Pit Dweller/Assets/Scripts/Player/playerMovement.cs
@@ -5,23 +5,40 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour {
     public float moveSpeed;
     public float jumpH;
+    public float dashSpeed;
+    public float startDash;
 
+    private float dashTime;
     private Rigidbody2D rb;
     private bool grounded;
 
     void Start()
     {
+        dashTime = startDash;
         rb = GetComponent<Rigidbody2D>();
     }
     
     void Update () {
+        //MOVEMENT
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
         }
-        if (Input.GetKeyDown(KeyCode.W) && grounded == true || Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        //JUMP
+        if (Input.GetKeyDown(KeyCode.W) && grounded == true)
         {
             rb.AddForce(new Vector2(0, jumpH), ForceMode2D.Impulse);
+        }
+        //DASH
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dashTime -= Time.deltaTime;
+            if (dashTime <= 0)
+            {
+                dashTime = startDash;
+                rb.velocity = Vector2.zero;
+            }
+            Dash();
         }
     }
 
@@ -37,6 +54,24 @@ public class playerMovement : MonoBehaviour {
         if (collision.gameObject.CompareTag("Ground"))
         {
             grounded = false;
+        }
+    }
+
+    void Dash()
+    {
+
+        Vector3 playerScreenPoint = GetComponentInParent<Transform>().position;
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        if (mousePosition.x < playerScreenPoint.x)
+        {
+            rb.velocity = Vector2.left * dashSpeed;
+        }
+        else
+        {
+            rb.velocity = Vector2.right * dashSpeed;
         }
     }
 }
