9481453111c27b61a4079ec54cd178fd3a8384ed
Howarddful123 <Howarddful123@gmail.com>
2019-03-23 16:17:59 -0700

    -final

---
+++ Pit Dweller/Assets/Scripts/sceneChanger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class sceneChanger : MonoBehaviour {
+
+    public void PlayAgain(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
